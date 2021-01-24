6b0f40c6f6e074b0b640f2c8c02a81c460914a66
Gary Lee <55985397+skaz93@users.noreply.github.com>
2021-01-24 21:57:22 +0800

    test4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d("MainActivity", "Attack of the killer androids");
         //test 12345
     }
 }
